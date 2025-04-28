d4c0f060c88c6f8a734c6b4a2f03228cad13ee60
Tony Enerson <tony@enerson.ca>
2025-04-28 10:52:09 -0600

    prep for deploy part 1

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(showRequests)
 app.use(express.json());
 
+app.use(express.static('public'))
+
 app.use('/api/playgroundEquipment', playgroundEquipmentRoutes);
 
 const server = app.listen(PORT, () => {
